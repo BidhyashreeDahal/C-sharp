74d5a656bc232dd3481326fe7c23065552932774
BidhyashreeDahal-11 <bidhyashree.dahal@dcmail.ca>
2024-11-06 14:36:31 -0500

    Update

---
+++ Cosc2100Demos/Week 8/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Week_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nud1 = new System.Windows.Forms.NumericUpDown();
+            this.cboOperator = new System.Windows.Forms.ComboBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.nud2 = new System.Windows.Forms.NumericUpDown();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nud1
+            // 
+            this.nud1.Location = new System.Drawing.Point(483, 173);
+            this.nud1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.nud1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nud1.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.nud1.Name = "nud1";
+            this.nud1.Size = new System.Drawing.Size(213, 39);
+            this.nud1.TabIndex = 0;
+            // 
+            // cboOperator
+            // 
+            this.cboOperator.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboOperator.FormattingEnabled = true;
+            this.cboOperator.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "%",
+            "x^y",
+            "sqrt"});
+            this.cboOperator.Location = new System.Drawing.Point(72, 173);
+            this.cboOperator.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.cboOperator.Name = "cboOperator";
+            this.cboOperator.Size = new System.Drawing.Size(212, 40);
+            this.cboOperator.TabIndex = 1;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMessage.ForeColor = System.Drawing.Color.Red;
+            this.lblMessage.Location = new System.Drawing.Point(57, 492);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(1072, 193);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "-";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 315);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "=";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // nud2
+            // 
+            this.nud2.Location = new System.Drawing.Point(483, 244);
+            this.nud2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.nud2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nud2.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.nud2.Name = "nud2";
+            this.nud2.Size = new System.Drawing.Size(213, 39);
+            this.nud2.TabIndex = 4;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(483, 348);
+            this.lblAnswer.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(30, 32);
+            this.lblAnswer.TabIndex = 5;
+            this.lblAnswer.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1269, 765);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.nud2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.cboOperator);
+            this.Controls.Add(this.nud1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nud1;
+        private System.Windows.Forms.ComboBox cboOperator;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown nud2;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
