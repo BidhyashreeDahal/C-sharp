0b5ad427ceb8d93fc9f77ef6ac4a6464d86b40f0
BidhyashreeDahal-11 <bidhyashree.dahal@dcmail.ca>
2024-12-14 21:49:33 -0500

    Commint

---
+++ Assignments/Final Project/Test.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Final_Project.DBAL;
+using System;
+
+namespace Final_Project
+{
+    [TestFixture]
+    public class ContactTests
+    {
+        private Contact _testContact;
+
+        [SetUp]
+        public void Setup()
+        {
+            _testContact = new Contact
+            {
+                ContactID = 1,
+                UserID = 1,
+                FullName = "Test User",
+                PhoneNumber = "+1234567890",
+                Email = "testuser@example.com",
+                Address = "123 Test Street",
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+        }
+
+        [Test]
+        public void InsertContacts_ValidData_ReturnsTrue()
+        {
+            bool result = Contact.InsertContacts(_testContact);
+            Assert.IsTrue(result, "InsertContacts failed.");
+        }
+
+        [Test]
+        public void GetContactByID_ValidID_ReturnsContact()
+        {
+            Contact.InsertContacts(_testContact);
+
+            var result = Contact.GetContactByID(_testContact.ContactID);
+            Assert.IsNotNull(result, "Contact not found.");
+            Assert.AreEqual(_testContact.ContactID, result.ContactID, "IDs do not match.");
+        }
+    }
+}
